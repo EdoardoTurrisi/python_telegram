1907dcef2c8adb533671bcd32fde6bbaf33f3e3b
EdoardoTurrisi <136438573+EdoardoTurrisi@users.noreply.github.com>
2023-06-16 11:23:44 +0200

    My first telegram bot
    My first telegram bot

---
+++ esempio_bot_telegram.py
@@ -0,0 +1,25 @@
+#esempio_bot_telegram
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+with open("token.txt", "r") as f:
+    TOKEN = f.read()
+    print(TOKEN)
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("hello", hello_funct))
+    app.add_handler(CommandHandler("Waddup", waddup_funct))
+    app.run_polling()
+
+async def hello_funct(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    hello_var = update.effective_user.first_name
+    await update.message.reply_text("Hello %s" % hello_var)
+
+async def waddup_funct(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    waddup_var = update.effective_user.first_name
+    await update.message.reply_text("Waddup %s, fr no cap" % waddup_var)
+
+if __name__ == '__main__':
+    main()
+    
